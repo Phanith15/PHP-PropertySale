e290cd66b05b8ace8c48a9e6e817d39abb30c902
Phanith15 <pnlovenz@gmail.com>
2016-04-06 08:28:28 +1200

    Edit home.php
    Edit features in home.php page

--- RealEstate/home.php
+++ RealEstate/home.php
@@ -1,27 +1,49 @@
     <!--<div class="bodycontainer"> <!--bodycontainer-->
-<div class="row no-padding no-margin" style="width: 100%">
+<div class="row" style="margin-top:40px;">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <p class="font-16px">
+            <span class="nectar-dropcap" style="color:#1e83ec;">S</span>
+            eparated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+            The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen.
+            She packed her seven versalia, put her initial into the belt and made herself on the way.
+            l using her.Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.
+        </p>
+        <h2>But nothing the copy said could convince her</h2>
+        <h2>and so it didn’t take long until a few insidious</h2>
+        <h2>Copy Writers ambushed her</h2> <br><br>
+        <p class="font-16px">
+            The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen.
+            She packed her seven versalia, put her initial into the belt and made herself on the way.
+            When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.
+            Pityful a rethoric question ran over her cheek, then she continued her way.
+            On her way she met a copy.
+        </p>
+    </div>
+</div>
+<div class="row pad-bottom" style="margin-top:20px;">    
     <div class="col-xs-12 col-sm-6 col-md-5 col-lg-4 " style="margin-left:0px;">
         <img src="images/2.jpg" width="100%" height="180">
             <h2><a href="">Queentown</a></h2>
-            <p>
+            <p class="font-16px">
                 Within a day or two’s drive you can see spectacular glaciers, picturesque fiords, rugged mountains, vast plains,
                 rolling hills, subtropical forests, a volcanic plateau, and miles of coastline with sandy beaches.
                 Much of these landscapes are protected by National Parks with thousands of kilometres of walks and trails opening their beauty to the public. 
             </p>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-5 col-lg-4">
-        <img src="images/2.jpg" width="100%" height="180">
-            <h2><a href="">Queentown</a></h2>
-            <p>
+        <img src="images/3.jpg" width="100%" height="180">
+            <h2><a href="">Samsung</a></h2>
+            <p class="font-16px">
                 Within a day or two’s drive you can see spectacular glaciers, picturesque fiords, rugged mountains, vast plains,
                 rolling hills, subtropical forests, a volcanic plateau, and miles of coastline with sandy beaches.
                 Much of these landscapes are protected by National Parks with thousands of kilometres of walks and trails opening their beauty to the public. 
             </p>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-5 col-lg-4">
-        <img src="images/2.jpg" width="100%" height="180">
-            <h2><a href="">Queentown</a></h2>
-            <p>
+        <img src="images/4.jpg" width="100%" height="180">
+            <h2><a href="">iPhone</a></h2>
+            <p class="font-16px">
                 Within a day or two’s drive you can see spectacular glaciers, picturesque fiords, rugged mountains, vast plains,
                 rolling hills, subtropical forests, a volcanic plateau, and miles of coastline with sandy beaches.
                 Much of these landscapes are protected by National Parks with thousands of kilometres of walks and trails opening their beauty to the public. 
@@ -61,21 +83,21 @@
                     </div>
                     
                     <div>
-                        <p>
+                        <p class="font-16px">
                         <a href=""><img style="margin-right:20px" src="images/News/iPhone6s.jpg" width="150" height="100" id="iphoneImg"></a>
                         If you're looking for a great deal on an iPhone 6S before Christmas, this is a bit of a cracker.
                         Mobiles.co.uk (owned by Carphone Warehouse) and O2 are currently running a great promotion on the best-of-the-best iPhone 6S 128GB which allows you to save £150 on the up front cost of the phone and then get yourself on a good value contract with O2.
                         </p>
                     </div>
                     <div>
-                        <p>
+                        <p class="font-16px">
                         <a href=""><img style="margin-right:20px" src="images/News/galaxy-s6-edge.jpg" width="150" height="100" id="galaxyImg"></a>
                         If you're looking for a great deal on an iPhone 6S before Christmas, this is a bit of a cracker.
                         Mobiles.co.uk (owned by Carphone Warehouse) and O2 are currently running a great promotion on the best-of-the-best iPhone 6S 128GB which allows you to save £150 on the up front cost of the phone and then get yourself on a good value contract with O2.
                         </p>
                     </div>
                     <div>
-                        <p>
+                        <p class="font-16px">
                         <a href=""><img style="margin-right:20px" src="images/News/iPad.jpg" width="150" height="100" id="ipadImg"></a>
                         If you're looking for a great deal on an iPhone 6S before Christmas, this is a bit of a cracker.
                         Mobiles.co.uk (owned by Carphone Warehouse) and O2 are currently running a great promotion on the best-of-the-best iPhone 6S 128GB which allows you to save £150 on the up front cost of the phone and then get yourself on a good value contract with O2.
@@ -84,5 +106,4 @@
                 </div>
             <!--</div>-->
         </div>
-        
 </div>
